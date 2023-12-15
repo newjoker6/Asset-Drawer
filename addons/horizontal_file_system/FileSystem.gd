27fd21c2af257cc85392c6dec603f5a880857abd
Michael McGuire <newjoker6@gmail.com>
2023-12-15 08:02:40 -0800

    Update FileSystem.gd

--- addons/horizontal_file_system/FileSystem.gd
+++ addons/horizontal_file_system/FileSystem.gd
@@ -1,38 +1,17 @@
 @tool
 extends EditorPlugin
 
-# Vertical children to convert to Horizontal
-var childVBox: int = 0
-var childVSplitter: int = 3
-
-# Separation size for the splitter you grab
-var separationAmount: int = 10
-
-# Initial spacing for the first item, filetree
-var initialSplit: int = 250
-
 # Padding from the bottom when popped out
 var padding: int = 20
 
 # The file system
 var FileDock: Object
 
-# Horizontal objects to replace the Vertical ones
-var hBox: HBoxContainer = HBoxContainer.new()
-var hSplitter: HSplitContainer = HSplitContainer.new()
-
-# Used to center window the first time you pop it out
-var initialLoad: bool = false
-
-# New size used for when the windows resize
-var newSize: Vector2
-
 # Toggle for when the file system is moved to bottom
 var filesBottom: bool = false
 
-# Store vertical children for when the plugin is disabled
-var fileVBox
-var fileVSplitter
+var newSize: Vector2
+var initialLoad: bool = false
 
 func _enter_tree() -> void:
 	# Add tool button to move shelf to editor bottom
@@ -40,24 +19,11 @@ func _enter_tree() -> void:
 	
 	# Get our file system
 	FileDock = self.get_editor_interface().get_file_system_dock()
-	
-	# Replace vertical elements with horizontal ones and set parameters
-	fileVBox = FileDock.get_child(childVBox)
-	FileDock.get_child(childVBox).replace_by(hBox, true)
-	
-	hSplitter.split_offset = initialSplit
-	hSplitter.add_theme_constant_override("separation", separationAmount)
-	hSplitter.size = Vector2(1280, FileDock.get_window().size.y)
-	
-	fileVSplitter = FileDock.get_child(childVSplitter)
-	FileDock.get_child(childVSplitter).replace_by(hSplitter, true)
 
 
 func _exit_tree() -> void:
 	remove_tool_menu_item("Files to Bottom")
 	remove_control_from_bottom_panel(FileDock)
-	FileDock.get_child(childVBox).replace_by(fileVBox)
-	FileDock.get_child(childVSplitter).replace_by(fileVSplitter)
 	if (FileDock.get_window().name != "root"):
 		FileDock.get_window().emit_signal("close_requested")
 	if(not filesBottom):
